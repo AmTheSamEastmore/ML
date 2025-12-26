5e2194b10f19cb2d13c496c4a913d7c09959a2d9
AmTheSamEastmore <316vane@gmail.com>
2025-12-26 16:06:06 -0300

    Fixed some errors V-2.2.2

--- lib.py
+++ lib.py
@@ -13,14 +13,14 @@ class Shortcut:
         self.letters = [chr(x) for x in range(97, 123)]
         self.block = chr(0x2586)
 
-    def sleep(self,time:float):
+    def sleep(self,secs:float):
         """Waits in seconds"""
-        time.sleep(time)
+        time.sleep(secs)
 
-    def delay(self,time:float):
+    def delay(self,milis:float):
         """Waits in miliseconds"""
-        time = time/1000
-        time.sleep(time)
+        milis = milis/1000
+        time.sleep(milis)
     
     def fastconfig(self,window:object,background:str = 'grey',title:str = 'Window',size:str = '500x500'):
         window.config(bg=background)
@@ -45,8 +45,8 @@ class Shortcut:
         '''Chooses a random thing from list'''
         return random.choice(list)
 
-    def on(self,condition:bool,func:function):
-        '''if condition is true, executes func'''
+    def on(self,condition:bool,func):
+        '''If condition is true, executes func'''
         if (condition):
             func()
 
@@ -70,13 +70,13 @@ class Shortcut:
         play = wave.play()
         play.wait_done()
     
-    def slowPrint(self, message:str, upper:bool = True, delay:int = 0.5):
+    def slowPrint(self, message:str, upper:bool = True, delay:float = 0.5):
         '''It\'s a print that waits in between printing each letter, and if you set upper to True it leaves spaces between letters and makes them uppercase'''
         for letter in message:
             if upper:
-                print(letter.capitalize(),end=' ')
+                print(letter.capitalize(),end=' ', flush=True)
             else:
-                print(letter,end='')
+                print(letter,end='', flush=True)
             time.sleep(delay)
         print('')
     
@@ -89,25 +89,28 @@ class Shortcut:
         tore = f'{str(randint(least,max))}.'
         for _ in range(decimals):
             tore = f'{tore}{randint(0,9)}'
+        return tore
     
 class Updates:
     ''' Works like an update func, but more organized, do addFunc(func) to add a function and start it with nameOfObj(), delay is the seconds that pass between each execution of funcs '''
-    def __init__(self, delay):
+    def __init__(self, miliDelay:float = 500):
         self.funcList = []
-        self.delay = delay
+        self.delay = miliDelay/1000
 
     def doFuncs(self):
         for func in self.funcList:
             func()
 
     def addFunc(self, func):
+        '''Adds a func to the list of functions that will get executed'''
         self.funcList.append(func)
 
     def updateStart(self):
         while True:
-            self.funcs()
+            self.doFuncs()
             time.sleep(self.delay)
 
     def __call__(self):
-        thrd = Thread(target=self.updating, daemon=False)
+        '''Starts the cicle'''
+        thrd = Thread(target=self.updateStart, daemon=False)
         thrd.start()
